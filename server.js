1daa1e4479c66e8be82513ffec6ca04fb64eb531
Tyson <tys_p@yahoo.ca>
2018-08-22 12:13:26 -0400

    made html pages and basic express server setup

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var parser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(parser.urlencoded({ extended: true }));
+app.use(parser.json());
+
+app.listen(PORT, function () {
+    console.log("FriendFinder listening on port " + PORT);
+});
